966d81995d767389c1f285f5fa45adb919dac741
franciscoldb <fluis905@gmail.com>
2022-10-26 21:59:26 -0300

    verificando resposta correta e pontuacao

--- Quiz.js
+++ Quiz.js
@@ -6,10 +6,13 @@ function comecar() {
 
 let timer = document.querySelector('#timer');
 function updateTimer(){
-    console.log(timer.innerHTML);
     timer.innerHTML--;
     if(timer.innerHTML <= 0)
         clearInterval(t);
+}
+let j = 5;
+function updateAfter(){
+    j--;
 }
 let t = setInterval(updateTimer, 1000);
 
@@ -19,6 +22,7 @@ const questao1 = {
     r2: "Robert Souza Tennyson",
     r3: "Ben Silva Tennyson",
     r4: "Benio Tenia Neves",
+    gabarito: 1,
     img: "./Images/ben10.jpg"
 }
 const questao2 = {
@@ -27,6 +31,7 @@ const questao2 = {
     r2: "Irma",
     r3: "Amiga",
     r4: "Namorada",
+    gabarito: 1,
     img: "./Images/gwen.webp"
 }
 const questao3 = {
@@ -35,14 +40,16 @@ const questao3 = {
     r2: "XRL80",
     r3: "CB160",
     r4: "RTX1080",
+    gabarito: 1,
     img: "./Images/XLR8.webp"
 }
 const questao4 = {
     pergunta: "Qual o nome do alien da figura abaixo?",
-    r1: "Massa Cinzenta",
+    r1: "Ultra-T",
     r2: "XLR8",
-    r3: "Ultra-T",
+    r3: "Massa Cinzenta",
     r4: "Insectóide",
+    gabarito: 1,
     img: "./Images/ultraT.jpg"
 }
 const questao5 = {
@@ -51,24 +58,53 @@ const questao5 = {
     r2: "Toguro",
     r3: "Alfred",
     r4: "Aku",
+    gabarito: 1,
     img: "./Images/ben102.jpg"
 }
 
 let listaPerguntas = [questao1, questao2, questao3, questao4, questao5];
 let enunciado = document.querySelector('#pergunta');
-let resposta1 = document.querySelector('#resp1');
+let resposta1 = document.getElementById('resp1');
 let resposta2 = document.querySelector('#resp2');
 let resposta3 = document.querySelector('#resp3');
 let resposta4 = document.querySelector('#resp4');
+let listaRespostas = [resposta1, resposta2, resposta3, resposta4];
 let imagem = document.querySelector('#imagem');
+let pontuacao = 0;
+let numero_questao = 0;
 
 function inicia_questao(n) {
-    enunciado.innerHTML = listaPerguntas[n-1].pergunta;
-    resposta1.innerHTML = listaPerguntas[n-1].r1;
-    resposta2.innerHTML = listaPerguntas[n-1].r2;
-    resposta3.innerHTML = listaPerguntas[n-1].r3;
-    resposta4.innerHTML = listaPerguntas[n-1].r4;
-    imagem.src = listaPerguntas[n-1].img;
+    enunciado.innerHTML = listaPerguntas[n].pergunta;
+    resposta1.innerHTML = listaPerguntas[n].r1;
+    resposta2.innerHTML = listaPerguntas[n].r2;
+    resposta3.innerHTML = listaPerguntas[n].r3;
+    resposta4.innerHTML = listaPerguntas[n].r4;
+    imagem.src = listaPerguntas[n].img;
 }
 
-inicia_questao(5);
+inicia_questao(numero_questao);
+
+function verifica_questao(resposta){
+    if(resposta === listaPerguntas[numero_questao].gabarito){
+        pontuacao++;
+    }
+    /* deixa em cinza as questoes erradas
+    for(let i = 0; i < listaRespostas.length; i++){
+        if(numero_questao != i)
+            listaRespostas[i].style.backgroundColor = 'rgba(117, 108, 108, 0.699)'; 
+    }
+    */
+    clearInterval(t);
+    j = 5;
+    let o = setInterval(updateTimer, 1000);
+    
+    if(numero_questao < 4){
+        console.log("Resposta: " + resposta);
+        console.log("Gabarito: " + listaPerguntas[numero_questao].gabarito);
+        console.log("Pontuacao: " + pontuacao);
+
+        timer.innerHTML = 20;
+        t = setInterval(updateTimer, 1000);
+        inicia_questao(++numero_questao);
+    }
+}
